a020394ee3447795e911e0229b86de8c54c64633
Mattias Larsson <mattias.larsson2016@gmial.com>
2023-08-07 19:24:47 +0200

    Breadth fixed and display improved

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
 import os
 import time
 import math
+import copy
 
 os.environ["SDL_AUDIODRIVER"] = "dsp"
 
@@ -17,44 +18,45 @@ class Display:
     def __init__(self, maze):
         self.maze = maze
         pg.init()
-        self.size = 1000
-        self.screen = pg.display.set_mode((self.size, self.size))
+        self.screen_size = 1000
+        self.size = self.screen_size / len(self.maze[0])
+        self.screen = pg.display.set_mode((self.screen_size, self.screen_size))
         pg.display.set_caption("Maze")
+        self.p = p_dfs
         self.exit = False
         self.first = False
         self.nbr_painted = 0
         self.n = 0
 
-    def start(self, p):
-        self.nbr_moves = int(math.ceil(p.nbr_moves / 255))
+    def start(self):
         while not self.exit:
-            self.run(p)
+            self.run()
 
-    def run(self, p):
+    def run(self):
         def animate():
-            if p.visited_order != []:
-                x, y = p.visited_order[0]
-                if len(p.visited_order) > 1:
-                    p.visited_order = p.visited_order[1:]
-                else:
-                    p.visited_order = []
-                if self.maze[y][x] != 2 and self.maze[y][x] != 3:
-                    self.maze[y][x] = 6
-                    size = self.size / len(self.maze[y])
-                    rect = pg.Rect(size * x, size * y, size, size)
-                    pg.draw.rect(
-                        self.screen,
-                        pg.Color(
-                            self.nbr_painted, 255 - self.nbr_painted, 255-int(self.nbr_painted/2)
-                        ),
-                        rect,
-                    )
-                    self.n += 1
-                    if self.n >= self.nbr_moves:
-                        self.nbr_painted += 1
-                        self.n = 0
-                    if self.nbr_painted == 255:
-                        self.nbr_painted = 0
+            if self.p.visited_order != []:
+                x, y = self.p.visited_order[0]
+                self.p.visited_order = (
+                    [] if len(self.p.visited_order) <= 1 else self.p.visited_order[1:]
+                )
+                if self.maze[y][x] in [2, 3]:
+                    return
+                rect = pg.Rect(self.size * x, self.size * y, self.size, self.size)
+                pg.draw.rect(
+                    self.screen,
+                    pg.Color(
+                        self.nbr_painted,
+                        255 - self.nbr_painted,
+                        255 - int(self.nbr_painted / 2),
+                    ),
+                    rect,
+                )
+                self.n += 1
+                if self.n % int(math.ceil(self.p.nbr_moves / 255)) == 0:
+                    self.nbr_painted += 1
+                    self.n = 0
+                if self.nbr_painted == 255:
+                    self.nbr_painted = 0
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -62,8 +64,7 @@ class Display:
         if not self.first:
             for i in range(len(self.maze)):
                 for j in range(len(self.maze[i])):
-                    size = self.size / len(self.maze[j])
-                    rect = pg.Rect(size * j, size * i, size, size)
+                    rect = pg.Rect(self.size * j, self.size * i, self.size, self.size)
                     if self.maze[i][j] == 0:
                         pg.draw.rect(self.screen, pg.Color(100, 40, 150), rect)
                     if self.maze[i][j] == 1:
@@ -72,27 +73,47 @@ class Display:
                         pg.draw.rect(self.screen, pg.Color(150, 0, 0), rect)
                     if self.maze[i][j] == 3:
                         pg.draw.rect(self.screen, pg.Color(0, 255, 0), rect)
-        self.first = True
+            self.first = True
         last = current_millis()
         current = last
-        while current - last < 15:
+        while current - last < 5:
             current = current_millis()
         animate()
+
+        def reset_display():
+            self.first = False
+            self.n = 0
+            self.nbr_painted = 0
+            self.screen.fill((0, 0, 0))
+
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
             self.exit = True
-        # if keys[pg.K_s]:
-        #    last = current_millis()
-        #    current = last
-        #    while current - last < 40:
-        #        current = current_millis()
-        #    animate()
+        if keys[pg.K_a]:  # Breadth First
+            self.p = p_bfs
+            self.p.visited_order = p_visited_backup_bfs
+            reset_display()
+        if keys[pg.K_d]:  # Depth First
+            self.p = p_dfs
+            self.p.visited_order = p_visited_backup_dfs
+            reset_display()
         pg.display.update()
 
 
-_maze = maze.get_maze()
-display = Display(_maze.maze)
-pl = player.Player(_maze.start.x, _maze.start.y, _maze)
-p = player.DepthFirst(pl)
-p.init()
-display.start(p)
+maze_dfs = maze.get_maze()
+maze_bfs = copy.deepcopy(maze_dfs)
+
+pl_dfs = player.Player(maze_dfs.start.x, maze_dfs.start.y, maze_dfs)
+pl_bfs = player.Player(maze_bfs.start.x, maze_bfs.start.y, maze_bfs)
+
+p_dfs = player.DepthFirst(pl_dfs)
+p_bfs = player.BreadthFirst(pl_bfs)
+
+p_dfs.init()
+p_bfs.init()
+
+p_visited_backup_dfs = [p for p in p_dfs.visited_order]
+p_visited_backup_bfs = [p for p in p_bfs.visited_order]
+
+display = Display(maze_dfs.maze)
+display.start()
--- player.py
+++ player.py
@@ -56,6 +56,7 @@ class DepthFirst:
         self.stack.append(current)
         self.run()
         finish_time = current_millis()
+        print("---------Depth First ---------")
         print(f"Total time: {finish_time-start_time}")
         print(f"Total nbr of moves: {self.nbr_moves}")
 
@@ -68,37 +69,26 @@ class BreadthFirst:
         self.visited = []
         self.visited_order = []
 
-    def run(self, depth):
-        if depth == 0:
-            return False
-        positions = self.pl.get_view()
-        self.stack.extend(positions)
-        for pos in positions:
-            x, y = pos
-            if self.pl.maze.maze[y][x] not in [2, 3]:
-                print(f"added {(x, y)}")
-                self.pl.maze.maze[y][x] = 5
-            self.nbr_moves += 1
+        self.stack = [(pl.x, pl.y)]
+
+    def run(self):
+        for pos in self.stack:
+            if pos in self.visited:
+                continue
             self.pl.x, self.pl.y = pos
-            self.stack.remove(pos)
+            self.visited.append(pos)
+            self.visited_order.append(pos)
+            self.nbr_moves += 1
             if self.pl.check_win():
                 return True
-        self.visited.extend(positions)
-        for pos in positions:
-            if self.run(depth - 1):
-                return True
+            self.stack.extend(self.pl.get_view())
 
     def init(self):
         start_time = current_millis()
-        self.stack = []
-        self.visited = []
-        current = (self.pl.x, self.pl.y)
-        self.visited.append(current)
-        self.stack.append(current)
-        depth = 0
-        while self.stack != [] and not self.pl.check_win():
-            self.run(depth)
-            depth += 1
+        while self.stack != []:
+            if self.run():
+                break
         finish_time = current_millis()
+        print("---------Breadth First ---------")
         print(f"Total time: {finish_time-start_time}")
         print(f"Total nbr of moves: {self.nbr_moves}")
